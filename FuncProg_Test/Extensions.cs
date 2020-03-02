028d22f65100a1ac6fcbf5cffaba1e3bec0606c5
rashif_ilmi <rashif.ilmi@gigaming.io>
2020-03-02 17:47:42 +0700

    just commit

---
+++ FuncProg_Test/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace FuncProg_Test
+{
+    public static class IEnumerableExtension
+    {
+        public static string Capitalize(this string values)
+        {
+            return (CultureInfo.CurrentCulture.TextInfo.ToTitleCase(values.ToLower()));
+        }
+        public static string SnakeCase(this string values)
+        {
+            return values.Replace(" ", "_");
+        }
+        public static string KebabCase(this string values)
+        {
+            return values.Replace(" ", "-");
+        }
+        public static T Most<T>(this IEnumerable <T> values)
+        {
+            return (values.GroupBy(X => X).OrderByDescending(X => X.Count()).Select(X=>X.Key)).First();
+        }
+        public static string TrimS(this string values, int num)
+        {
+            return $"{values.Substring(0, num)}...";
+        }
+        public static string TrimW(this string values, int num)
+        {
+            return String.Join(" ",values.Split().Take(num));
+        }
+    }
+}
---
+++ FuncProg_Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FuncProg_Test
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var sentence = "heloo darkness my old friend";
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 6, 8, 8, 6, 9 };
+            var grades = new[] { 87.5, 88.5, 60.5, 90.5, 88.5 };
+            var words = new[] {"abc","abc","abc","cab","cba","bca" };
+            //num1
+            Console.WriteLine(sentence.Capitalize());
+            //num2
+            Console.WriteLine(sentence.SnakeCase());
+            //num3
+            Console.WriteLine(sentence.KebabCase());
+            //num4
+            Console.WriteLine(numbers.Most());
+            Console.WriteLine(grades.Most());
+            Console.WriteLine(words.Most());
+            //num6
+            Console.WriteLine(sentence.TrimS(8));
+            //num7
+            Console.WriteLine(sentence.TrimW(3));
+        }
+    }
+
+
+    
+}
